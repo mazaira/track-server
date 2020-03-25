3c7dad4e4ef7247b7b35c251daec5295221e836f
alberto <a.mmazaira@gmail.com>
2020-03-25 10:57:24 +0100

    signin

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -12,12 +12,31 @@ router.post('/signup', async (req, res) => {
 
     await user.save();
 
-    const token = jwt.sign({ userId: user._id }, 'SECRET_KEY' )
+    const token = jwt.sign({ userId: user._id }, 'SECRET_KEY')
 
     res.send({ token: token });
   } catch (err) {
     return res.status(422).send(err.message);
   }
 });
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({error: 'must provide email and pass'});
+  }
+
+  const user = await User.findOne({ email });
+  if(!user) {
+    return res.status(404).send({ error: 'Email or password invalid'});
+  }
+  try{
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'SECRET_KEY')
+    res.send({token});
+  } catch(err) {
+    return res.status(404).send({ error: 'Email or password invalid' })
+  }
+  });
 
 module.exports = router;
